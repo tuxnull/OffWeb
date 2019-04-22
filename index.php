ec24b882dbfc0f50c986dacba76001102df9e893
tuxnull <tuxnull@setstorm.com>
2019-04-22 22:38:43 +0200

    Changed "header" style name to "feature"

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-		<div class="header">OffWeb Hub | <?PHP echo date('l jS \of F Y h:i:s A');  ?></div>
+		<div class="feature">OffWeb Hub | <?PHP echo date('l jS \of F Y h:i:s A');  ?></div>
 		<div class="container">
 			<h1>Welcome to the OffWeb!</h1>
 			<p>An offline copy of the internet</p>
