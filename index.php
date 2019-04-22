50fefc2d005db218fe9c26b5025778d7a00d9c43
tuxnull <tuxnull@setstorm.com>
2019-04-22 22:32:57 +0200

    Added index and global stylesheet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>OffWeb Hub</title>
+		<link rel="stylesheet" href="glob_style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<div class="header">OffWeb Hub | <?PHP echo date('l jS \of F Y h:i:s A');  ?></div>
+		<div class="container">
+			<h1>Welcome to the OffWeb!</h1>
+			<p>An offline copy of the internet</p>
+		</div>
+		<?PHP
+			
+		?>
+	</body>
+</html>
